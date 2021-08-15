3cb971f6d65b2813b01290e50716d743d839dac7
Chung Kao <chungckao@gmail.com>
2021-08-15 13:18:16 +0800

    Complete Lists in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-const names = ["Chung", "Mary", "John"];
+import * as React from "react";
+
+const list = [
+	{
+		title: "React",
+		url: "https://reactjs.org/",
+		author: "Jordan Walke",
+		num_comments: 3,
+		points: 4,
+		objectID: 0,
+	},
+	{
+		title: "Redux",
+		url: "https://redux.js.org/",
+		author: "Dan Abramov, Andrew Clark",
+		num_comments: 2,
+		points: 5,
+		objectID: 1,
+	},
+];
 
 const App = () => (
 	<div>
-		<h1>Hello{names.map((name) => ` ${name}`)}!</h1>
+		<h1>My Hacker Stories</h1>
 		<label htmlFor='search'>Search: </label>
 		<input id='search' type='text' />
+		<hr />
+		<ul>
+			{/* make list items */}
+			{list.map((el) => (
+				<li key={el.objectID}>
+					<span>
+						<a href={el.url}>{el.title}</a>
+					</span>
+					<span>{el.author}</span>
+					<span>{el.num_comments}</span>
+					<span>{el.points}</span>
+				</li>
+			))}
+		</ul>
 	</div>
 );
 
