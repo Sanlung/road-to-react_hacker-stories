d35a8c50d0eda8ac4ee6f908fbaade5b53d990ea
Chung Kao <chungckao@gmail.com>
2021-08-31 18:05:58 +0800

    Complete Handler Functions, React Props, React State, and Callback Handlers

--- src/App.js
+++ src/App.js
@@ -1,54 +1,81 @@
 import * as React from "react";
 
-const list = [
-	{
-		title: "React",
-		url: "https://reactjs.org/",
-		author: "Jordan Walke",
-		num_comments: 3,
-		points: 4,
-		objectID: 0,
-	},
-	{
-		title: "Redux",
-		url: "https://redux.js.org/",
-		author: "Dan Abramov, Andrew Clark",
-		num_comments: 2,
-		points: 5,
-		objectID: 1,
-	},
-];
+const App = () => {
+	const stories = [
+		{
+			title: "React",
+			url: "https://reactjs.org/",
+			author: "Jordan Walke",
+			num_comments: 3,
+			points: 4,
+			objectID: 0,
+		},
+		{
+			title: "Redux",
+			url: "https://redux.js.org/",
+			author: "Dan Abramov, Andrew Clark",
+			num_comments: 2,
+			points: 5,
+			objectID: 1,
+		},
+	];
 
-const App = () => (
+	const handleSearch = (e) => {
+		console.log(e.target.value);
+	};
+
+	return (
+		<div>
+			<h1>My Hacker Stories</h1>
+			<Search onSearch={handleSearch} />
+			<hr />
+			<List list={stories} />
+		</div>
+	);
+};
+
+const List = (props) => (
 	<div>
-		<h1>My Hacker Stories</h1>
-		<Search />
-		<hr />
-		<List />
+		<h2>{props.title}</h2>
+		<ul>
+			{/* make list items */}
+			{props.list.map((el) => (
+				<Item key={el.objectID} item={el} />
+			))}
+		</ul>
 	</div>
 );
 
-const List = () => (
-	<ul>
-		{/* make list items */}
-		{list.map((el) => (
-			<li key={el.objectID}>
-				<span>
-					<a href={el.url}>{el.title}</a>
-				</span>
-				<span>{el.author}</span>
-				<span>{el.num_comments}</span>
-				<span>{el.points}</span>
-			</li>
-		))}
-	</ul>
+const Item = (props) => (
+	<li>
+		<span>
+			<a href={props.item.url}>{props.item.title}</a>
+		</span>
+		<span>{props.item.author}</span>
+		<span>{props.item.num_comments}</span>
+		<span>{props.item.points}</span>
+	</li>
 );
 
-const Search = () => (
-	<div>
-		<label htmlFor='search'>Search: </label>
-		<input id='search' type='text' />
-	</div>
-);
+const Search = (props) => {
+	const [searchTerm, setSearchTerm] = React.useState("");
+
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value);
+		props.onSearch(e);
+	};
+
+	return (
+		<div>
+			<label htmlFor='search'>Search: </label>
+			<input
+				id='search'
+				type='text'
+				placeholder={searchTerm}
+				onChange={handleChange}
+			/>
+		</div>
+	);
+};
 
 export default App;
